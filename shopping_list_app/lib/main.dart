502437a8fd543b814460ea499b93246afd3e1175
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2020-08-24 08:37:56 +0300

    just list with button adding more elements

--- shopping_list_app/lib/main.dart
+++ shopping_list_app/lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
+
+class Customer {
+  int _id;
+  String _name;
+
+  Customer(this._id, this._name);
+
+  Customer.empty() : this(0, "");
+
+  int get id => _id;
+
+  String get name => _name;
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -10,114 +24,44 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new HomePage(),
+      home: new ProductWidget(),
     );
   }
 }
 
-class ProductInfo {
-  String _name = "";
-
-  ProductInfo(this._name);
-
-  ProductInfo.empty();
-
-  String get name => name;
-
-  @override
-  String toString() {
-    return 'ProductInfo{_name: $_name}';
-  }
-}
-
-class HomePage extends StatefulWidget {
-  List<ProductInfo> _products = new List<ProductInfo>();
-
-  HomePage({Key key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => new _HomePageState(_products);
-}
-
-class _HomePageState extends State<HomePage> {
-  List<ProductInfo> _products;
-
-  _HomePageState(List<ProductInfo> _products) {
-    this._products = _products;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Shopping List"),
-        ),
-        body: new Center(
-            child: new ListView(children: [
-          Padding(
-              padding: EdgeInsets.all(20.0),
-              child: ProductWidget(products: _products, onSaved: _onSaved))
-        ])));
-  }
-
-  _onSaved(List<ProductInfo> products) {
-    showDialog<bool>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-              title: const Text("A"),
-              content: Text("B"),
-              actions: <Widget>[
-                FlatButton(
-                  onPressed: () {
-                    Navigator.pop(context, true);
-                  },
-                  child: const Text("B"),
-                )
-              ]);
-        });
-  }
-}
-
 class ProductWidget extends StatefulWidget {
-  List<ProductInfo> _products;
-  ValueChanged<List<ProductInfo>> _onSaved;
-
-  ProductWidget(
-      {Key key,
-      @required List<ProductInfo> products,
-      @required ValueChanged<List<ProductInfo>> onSaved})
-      : super(key: key) {
-    this._products = products;
-    this._onSaved = onSaved;
-  }
+  ProductWidget({Key key}) : super(key: key);
 
   @override
-  _ProductWidgetState createState() => new _ProductWidgetState(_products);
+  _ProductWidgetState createState() => new _ProductWidgetState();
 }
 
 class _ProductWidgetState extends State<ProductWidget> {
-  List<ProductInfo> _products;
-
-  _ProductWidgetState(final List<ProductInfo> products) {
-    _products = products;
-  }
+  List<String> _products = new List<String>();
 
   @override
   Widget build(BuildContext context) {
-    _products.add(new ProductInfo("a"));
-    ListView builder = ListView.builder(
-        itemCount: _products.length,
-        itemBuilder: (context, index) {
-          var g = _products[index];
-          return ListTile(title: Text('${g.name}'));
-        });
-
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Nasa Offices"),
-        ),
-        body: new Center(child: builder));
+      appBar: new AppBar(
+        title: new Text("ZZZ"),
+      ),
+      body: ListView.builder(
+          itemCount: _products.length,
+          itemBuilder: (context, index) {
+            return ListTile(title: new ListTile(title: Text(_products[index])));
+          }),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _addSomething,
+          backgroundColor: Colors.red,
+          child: new Icon(Icons.portable_wifi_off),
+      ),
+    );
+  }
 
+  void _addSomething() {
+    _products.add("something");
+    setState(() {
+
+    });
   }
 }
