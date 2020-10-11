a165af0b318036f5a2daf442e43c9b8ce4802dde
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2020-10-11 13:31:51 +0300

    not working delegate implementation

--- table/lib/render_my_table.dart
+++ table/lib/render_my_table.dart
@@ -1,11 +1,13 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
 import 'package:table/my_table_painter.dart';
 
-class RenderMyTable extends RenderBox {
-  RenderMyTable(this.children)
-  : _myTablePainter = MyTablePainter(children: children);
+class RenderMyTable extends RenderCustomMultiChildLayoutBox {
+  RenderMyTable({this.children, this.delegate})
+      : _myTablePainter = MyTablePainter(children: children),
+        super(delegate: delegate);
 
+  final MultiChildLayoutDelegate delegate;
   final MyTablePainter _myTablePainter;
 
   final List<Widget> children;
