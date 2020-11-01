f2028b290a63402783b1b55ec908ae3ed0148428
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2020-11-01 13:11:42 +0200

    variables for desktop

--- env.sh
+++ env.sh
@@ -1,2 +1,6 @@
 export ANDROID_SDK_ROOT=/opt/Android/SDK
 export PATH=$PATH:/opt/flutter/bin
+
+
+export ANDROID_SDK_ROOT=/media/yakovcha/non-encrypted/learning-flutter/Android/Sdk
+export PATH=$PATH:/media/yakovcha/non-encrypted/learning-flutter/flutter/bin
