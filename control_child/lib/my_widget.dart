7f5e84aa3c149a0cca175ce8a1c21197dc723a24
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2020-11-01 15:50:04 +0200

    stubbing is successful, app start without errors

--- control_child/lib/my_widget.dart
+++ control_child/lib/my_widget.dart
@@ -3,15 +3,57 @@ import 'package:flutter/widgets.dart';
 class MyWidget extends RenderObjectWidget{
 
   @override
-  RenderObjectElement createElement() {
-    // TODO: implement createElement
-    throw UnimplementedError();
+  MyWidgetElement createElement() {
+    return MyWidgetElement(this);
   }
 
   @override
-  RenderObject createRenderObject(BuildContext context) {
-    // TODO: implement createRenderObject
-    throw UnimplementedError();
+  RenderMyWidget createRenderObject(BuildContext context) {
+    return RenderMyWidget();
   }
+
+}
+
+class MyWidgetElement extends RenderObjectElement{
+  MyWidgetElement(MyWidget widget) : super(widget);
+
+  @override
+  MyWidget get widget => super.widget as MyWidget;
+
+  @override
+  RenderMyWidget get renderObject => super.renderObject as RenderMyWidget;
+
+  @override
+  void mount(Element parent, dynamic newSlot){
+    super.mount(parent, newSlot);
+  }
+}
+
+class RenderMyWidget extends RenderBox{
+  // @override
+  // void debugAssertDoesMeetConstraints() {
+  //   // TODO: implement debugAssertDoesMeetConstraints
+  // }
+
+  // @override
+  // // TODO: implement paintBounds
+  // Rect get paintBounds => throw UnimplementedError();
+
+  @override
+  void performLayout() {
+    final BoxConstraints constraints = this.constraints;
+    size = constraints.constrain(Size(100,100));
+  }
+
+
+
+  // @override
+  // void performResize() {
+  //   // TODO: implement performResize
+  // }
+  //
+  // @override
+  // // TODO: implement semanticBounds
+  // Rect get semanticBounds => throw UnimplementedError();
 
 }
