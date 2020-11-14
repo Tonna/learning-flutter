bec5b8003fa95c67fc85d24da8917d1c1c1729b6
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2020-11-14 10:30:34 +0200

    cleanup, and adding randomness for painting

--- control_child/lib/my_widget.dart
+++ control_child/lib/my_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
@@ -40,78 +42,51 @@ class _MyWidgetElement extends SingleChildRenderObjectElement {
 //}
 }
 
-class _RenderMyWidget extends RenderCustomSingleChildLayoutBox {
-  _RenderMyWidget() : super(delegate: _MySingeChildLayoutDelegate(100, true));
+class _RenderMyWidget extends RenderShiftedBox {
+  _RenderMyWidget() : super(null);
 
   @override
   void performLayout() {
-    var childSize = delegate.getSize(this.constraints);
+    var maxSize = Size(
+        constraints.biggest.width - 10,
+        constraints.biggest.height == double.infinity
+            ? 500
+            : constraints.biggest.height - 10);
 
+    var childSize =
+        Size(maxSize.width / rand(3, 5), maxSize.height / rand(3, 5));
     child.layout(
         BoxConstraints.tightFor(
             width: childSize.width, height: childSize.height),
         parentUsesSize: true);
-    //child.performLayout();
 
-    size =
-        constraints.constrain(Size(childSize.width * 3, childSize.height * 3));
+    size = constraints.constrain(
+        Size(childSize.width * rand(2, 2), childSize.height * rand(2, 2)));
+  }
+
+  int rand(int startFrom, int maxAdd) {
+    return Random().nextInt(maxAdd) + startFrom;
   }
 
   @override
   void paint(PaintingContext context, Offset offset) {
     final Paint paint = Paint()
-      ..color = const Color(0xFF33CC33)
+      ..color = Colors.red
       ..strokeWidth = 0.5
       ..style = PaintingStyle.stroke
       ..maskFilter = MaskFilter.blur(BlurStyle.solid, 0.5);
+
     context.canvas.drawRect(offset & size, paint);
 
     if (child != null) {
       final BoxParentData childParentData = child.parentData as BoxParentData;
 
-      var childOffset = Offset(offset.dx + 10.0, offset.dy + 70.0);
-      context.canvas.drawRect(
-          childOffset &
-              child.semanticBounds.size,
-          paint);
+      var childOffset = Offset(
+          offset.dx + rand(0, size.width.toInt() - child.size.width.toInt()),
+          offset.dy + rand(0, size.height.toInt() - child.size.height.toInt()));
+      context.canvas.drawRect(childOffset & child.semanticBounds.size, paint);
 
       context.paintChild(child, childParentData.offset + childOffset);
     }
-  }
-}
-
-//copypaste from _ModalBottomSheetLayout
-class _MySingeChildLayoutDelegate extends SingleChildLayoutDelegate {
-  _MySingeChildLayoutDelegate(this.progress, this.isScrollControlled);
-
-  final double progress;
-  final bool isScrollControlled;
-
-  @override
-  BoxConstraints getConstraintsForChild(BoxConstraints constraints) {
-    return BoxConstraints(
-      minWidth: constraints.maxWidth,
-      maxWidth: constraints.maxWidth,
-      minHeight: 0.0,
-      maxHeight: isScrollControlled
-          ? constraints.maxHeight
-          : constraints.maxHeight * 9.0 / 16.0,
-    );
-  }
-
-  @override
-  Offset getPositionForChild(Size size, Size childSize) {
-    return Offset(0.0, size.height - childSize.height * progress);
-  }
-
-  @override
-  bool shouldRelayout(_MySingeChildLayoutDelegate oldDelegate) {
-    return true;
-  }
-
-  @override
-  Size getSize(BoxConstraints constraints) {
-    // TODO: implement getSize
-    return Size(100, 40);
   }
 }
